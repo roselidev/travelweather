82eff160edab9dee66652c2035fe2194767d1926
shlee0916 <shlee0916.dev@gmail.com>
2024-08-11 11:25:09 +0900

    Feat: Add year option on /weather API, Change response

--- server/weather/service.py
+++ server/weather/service.py
@@ -30,18 +30,68 @@ def get_city_suggestion(start_month: int, end_month: int):
     return datas
     
     
-def get_specific_weather(country, city, start_month, end_month):
+def get_specific_weather(country, city, start_month, end_month, years=10):
+    def get_comment(good_score):
+        if good_score > 0.8:
+            return "Perfect days for travel! 🤞"
+        elif good_score > 0.2:
+            return "There might be a few rainy days, but still a great trip :D"
+        else:
+            return "Challenging days for travel! But it will still be memorable."
+
+    def get_emoji(prcp):
+        if prcp < 30:
+            return "fa fa-sun"
+        elif prcp < 50:
+            return "fa fa-cloud"
+        else:
+            return "fa fa-cloud-rain"
+    
+    datas = defaultdict(lambda: defaultdict(float))
     year = datetime.now().year
-    query_start = datetime(year - 1, start_month, 1)
-    query_end = datetime(year - 1, end_month, 1)
+
+    # Fetch weather data for the last 3 years
+    for i in range(1, years+1):
+        query_start = datetime(year - i, start_month, 1)
+        query_end = datetime(year - i, end_month, 28)  # Last day of the end month, 28 is safe for all months
     
-    weather_stations = Stations()
-    station = weather_stations.nearby(contries_cities[country][city][0], contries_cities[country][city][1]).fetch(1)
+        weather_stations = Stations()
+        station = weather_stations.nearby(contries_cities[country][city][0], contries_cities[country][city][1]).fetch(1)
     
-    datas = defaultdict(lambda: defaultdict(dict))
-    data = Monthly(station, query_start, query_end).fetch()
-    data = data.fillna("")
-    for time, weather in data.to_dict("index").items():
-        datas[country][city][str(time)[:7]] = weather
+        data = Monthly(station, query_start, query_end).fetch()
+        data = data.fillna(0)  # Fill missing data with 0
+        
+        for index, row in data.iterrows():
+            month = index.month
+            datas[month]['tavg'] += row['tavg']
+            datas[month]['tmin'] += row['tmin']
+            datas[month]['tmax'] += row['tmax']
+            datas[month]['prcp'] += row['prcp']
+
+    good_score = 0
+    for month in range(start_month, end_month + 1):
+        tavg = datas[month]['tavg'] / years
+        tmin = datas[month]['tmin'] / years
+        tmax = datas[month]['tmax'] / years
+        prcp = datas[month]['prcp'] / years
+        
+        datas[month] = {
+            'tavg': tavg,
+            'tmin': tmin,
+            'tmax': tmax,
+            'prcp': prcp,
+            'emoji': get_emoji(prcp)
+        }
+
+        if prcp < 30:
+            good_score += 1
+        elif prcp < 50:
+            good_score += 0.3
+        else:
+            good_score += 0.1
     
-    return datas
+    good_score /= (end_month - start_month + 1)
+
+    datas['comment'] = get_comment(good_score)
+        
+    return dict(datas)
